5688d81625d660e3e94acb0441565e8a10e19d0f
Archiehere <cheeku67@gmail.com>
2023-01-27 19:17:14 +0530

    removed oauth

--- src/Authentication/components/loginbox.jsx
+++ src/Authentication/components/loginbox.jsx
@@ -12,7 +12,7 @@ const Instruction = (props) => {
   return (
     <div id="auth_instruction">
       <pre className="line"></pre>
-      <span>Or {props.status} with email</span>
+      <span>{props.status} with email</span>
       <pre className="line"></pre>
     </div>
   );
@@ -29,10 +29,10 @@ const Oauth = (props ) => {
   return (
     <div id="loginbox">
       <p className="authbox">Welcome to ConnectPlus</p>
-      <button id="oauth" className="authbox">
-      <img id="googleicon" src={illustration} alt="google" />
-        <span><b>Continue with Google</b></span>
-      </button>
+      {/* <button id="oauth" className="authbox"> */}
+      {/* <img id="googleicon" src={illustration} alt="google" /> */}
+        {/* <span><b>Continue with Google</b></span> */}
+      {/* </button> */}
       <Instruction status={props.status} />
     </div>
   );
